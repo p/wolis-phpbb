c8a22aa277549ec3e91b1216e9879e493ba2206a
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-01 02:54:41 -0500

    Support testing all phpbb branches from the same source tree

--- test/wolis_test_case.py
+++ test/wolis_test_case.py
@@ -1,9 +1,11 @@
 import owebunit
+import os
 
 class WolisTestCase(owebunit.WebTestCase):
     def __init__(self, *args, **kwargs):
         super(WolisTestCase, self).__init__(*args, **kwargs)
         self.config.host = 'http://func'
+        self.flavor = os.environ['FLAVOR']
     
     def login(self, username, password):
         params = {

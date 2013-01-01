6605e991d4860b5a0c27b8e8501ca2fc3aea5fc1
Oleg Pudeyev <oleg@bsdpower.com>
2012-12-31 20:08:19 -0500

    Login test

---
+++ test/login.py
@@ -0,0 +1,27 @@
+import owebunit
+
+class LoginTestCase(owebunit.WebTestCase):
+    def __init__(self, *args, **kwargs):
+        super(LoginTestCase, self).__init__(*args, **kwargs)
+        self.config.host = 'http://func'
+    
+    def test_login(self):
+        self.get('/')
+        self.assert_status(200)
+        
+        assert 'Log out' not in self.response.body
+        
+        params = {
+            'username': 'morpheus',
+            'password': 'morpheus',
+            'login': 'Login',
+        }
+        
+        self.post('/ucp.php?mode=login', body=params)
+        self.assert_status(200)
+        
+        assert 'You have been successfully logged in.' in self.response.body
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()

efe8e4ced8145948a128b169183c5aaf9f58c285
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-01 00:19:20 -0500

    Login test now works

--- test/login.py
+++ test/login.py
@@ -21,6 +21,12 @@ class LoginTestCase(owebunit.WebTestCase):
         self.assert_status(200)
         
         assert 'You have been successfully logged in.' in self.response.body
+        
+        self.get('/')
+        #self.get('/viewforum.php?f=1')
+        self.assert_status(200)
+        
+        assert 'Logout' in self.response.body
 
 if __name__ == '__main__':
     import unittest

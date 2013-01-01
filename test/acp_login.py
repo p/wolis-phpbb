775c08484a29f97a19fb94d712334a5fe0c0b95d
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-01 02:27:19 -0500

    acp login test case

---
+++ test/acp_login.py
@@ -0,0 +1,50 @@
+import owebunit
+import urlparse
+from wolis_test_case import WolisTestCase
+
+class AcpLoginTestCase(WolisTestCase):
+    def test_login(self):
+        self.login('morpheus', 'morpheus')
+        
+        self.get('/')
+        self.assert_status(200)
+        
+        doc = self.response.lxml_etree
+        acp_link = self.xpath_first(doc, '//div[@class="copyright"]//a[text() = "Administration Control Panel"]').attrib['href']
+        acp_link = urlparse.urljoin('/', acp_link)
+        
+        self.get(acp_link)
+        self.assert_status(200)
+        
+        assert 'To administer the board you must re-authenticate yourself.' in self.response.body
+        
+        assert len(self.response.forms) == 2
+        form = self.response.forms[1]
+        
+        doc = self.response.lxml_etree
+        password_name = self.xpath_first(doc, '//input[@type="password"]').attrib['name']
+        
+        params = {
+            'username': 'morpheus',
+            password_name: 'morpheus',
+        }
+        
+        params = owebunit.extend_params(form.params.list, params)
+        self.post(form.computed_action, body=params)
+        self.assert_status(200)
+        
+        assert 'You have successfully authenticated' in self.response.body
+        
+        doc = self.response.lxml_etree
+        acp_link = self.xpath_first(doc, '//div[@class="copyright"]//a[text() = "Administration Control Panel"]').attrib['href']
+        # note: uses previous page's form's computed action
+        acp_link = urlparse.urljoin(form.computed_action, acp_link)
+        
+        self.get(acp_link)
+        self.assert_status(200)
+        
+        assert 'Board statistics' in self.response.body
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
--- test/wolis_test_case.py
+++ test/wolis_test_case.py
@@ -4,3 +4,26 @@ class WolisTestCase(owebunit.WebTestCase):
     def __init__(self, *args, **kwargs):
         super(WolisTestCase, self).__init__(*args, **kwargs)
         self.config.host = 'http://func'
+    
+    def login(self, username, password):
+        params = {
+            'username': username,
+            'password': password,
+            'login': 'Login',
+        }
+        
+        self.post('/ucp.php?mode=login', body=params)
+        self.assert_status(200)
+        
+        assert 'You have been successfully logged in.' in self.response.body
+    
+    def find_sid(self):
+        cookie_names = self._session._cookie_jar.keys()
+        sid_name = None
+        for name in cookie_names:
+            if name.endswith('_sid'):
+                if sid_name is None:
+                    sid_name = name
+                else:
+                    raise ValueError, 'Duplicate sid cookies detected: %s and %s' % (sid_name, name)
+        return self._session._cookie_jar[sid_name].value

d29b4e15243310dd18b218f4b83a8605640751a7
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-06 00:31:03 -0500

    Check that default timezone offered by the server side matches configured guest timezone

--- test/register.py
+++ test/register.py
@@ -51,6 +51,19 @@ class RegisterTestCase(WolisTestCase):
         assert 'Thank you for registering' in self.response.body
         # check that there are no further actions needed
         assert 'You may now login with your username' in self.response.body
+    
+    def test_initial_timezone_ascreus(self):
+        self.get('/ucp.php?mode=register&agreed=yes')
+        self.assert_successish()
+        
+        assert 'Username:' in self.response.body
+        
+        assert len(self.response.forms) == 2
+        form = self.response.forms[1]
+        assert form.params.dict['tz_date'].startswith('GMT+11:00')
+        
+        #doc = self.response.lxml_etree
+        #timezone_select = self.xpath_first(doc, 'select[@id="timezone"]')
 
 if __name__ == '__main__':
     import unittest
